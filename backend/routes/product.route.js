694ea98b229f6976f6213703130366c9cc94214b
parthivvora <v.parthiv004@gmail.com>
2024-03-07 12:07:25 +0530

    Fix: Create categories wise product API

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { addProducts, updateProduct, getAllProductData, updateProductStatus, getAllSingleProductData } = require("../controllers/product.controller")
+const { addProducts, updateProduct, getAllProductData, updateProductStatus, getAllSingleProductData, getCategoryProduct } = require("../controllers/product.controller")
 const { adminAuth } = require("../middleware/adminAuth")
 const productImageUpload = require("../middleware/productImageUpload")
 const router = express.Router()
@@ -8,6 +8,7 @@ router.post("/addProduct", adminAuth, productImageUpload.single("productImage"),
 router.put("/editProduct/:productId", adminAuth, productImageUpload.single("productImage"), updateProduct)
 router.get("/getAllProducts", getAllProductData)
 router.get("/getAllSingleProductData/:productId", getAllSingleProductData)
+router.get("/getCategoryProduct/:categoryId", getCategoryProduct)
 router.put("/updateProductStatus/:productId", adminAuth, updateProductStatus)
 
 module.exports = router
